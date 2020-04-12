a5fea270600104633d9872a9e72ea85f87530871
Lapras <jermurray2@gmail.com>
2020-04-12 17:00:25 -0400

    4/12 dev log and changes
    Added FTC Robotics page and baby subpages
    Added written content for FTC Robotics page

--- siteJS.js
+++ siteJS.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {phadeHeader()}
 
 function phadeHeader() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
         document.getElementById("header").style.opacity = "0";
     } else {
         document.getElementById("header").style.opacity = "1";
